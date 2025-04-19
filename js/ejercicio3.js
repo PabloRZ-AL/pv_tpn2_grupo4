ed8e5cf95250660aaf190d1c8715cfc9919a5f3b
AlexUnju <alexxe@fi.unju.edu.ar>
2025-04-19 14:43:39 -0300

    feat: Resolución ejercicio 3

---
+++ js/ejercicio3.js
@@ -0,0 +1,19 @@
+const radio = 7.5;
+
+// Calcular el área usando Math.PI para obtener el valor de π
+const area = Math.PI * Math.pow(radio, 2);
+
+// Mostrar el resultado por consola
+console.log("Datos del círculo:");
+console.log("Radio:", radio);
+console.log("Área:", area.toFixed(2)); // toFixed(2) es el metodo para mostrar solo 2 decimales
+
+//##############################
+// CONSOLA
+//##############################
+
+// LO QUE MUESTRA EN LA CONSOLA
+// Datos del círculo:
+// Radio: 7.5
+// Área: 176.71
+
