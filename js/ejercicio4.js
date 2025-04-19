e41159de30571b3ddc97923e08e46b51463639f3
AlexUnju <alexxe@fi.unju.edu.ar>
2025-04-19 14:35:01 -0300

    feat: Resolución ejercicio 4

---
+++ js/ejercicio4.js
@@ -0,0 +1,13 @@
+const lado = 5;
+
+// Calcular el área del cuadrado (lado * lado)
+const area = lado * lado;
+
+// Calcular el perímetro del cuadrado (4 * lado)
+const perimetro = lado * 4;
+
+// Mostrar los resultados por consola
+console.log("Datos del cuadrado:");
+console.log("Lado:", lado);
+console.log("Área:", area);
+console.log("Perímetro:", perimetro);
