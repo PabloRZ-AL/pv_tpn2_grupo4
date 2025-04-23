fbcc95a5291f43b3878819fcc36a676b86bbc83b
PabloRZ-AL <rodriguezpablouski.com>
2025-04-23 17:34:19 -0300

    punto nueve

--- js/ejercicio9.js
+++
@@ -1,9 +0,0 @@
-function mostrarMensaje() {
-    let nombre = document.getElementById("nombreUsuario").value;
-
-    if (nombre.trim() !== "") {
-        document.getElementById("mensaje").innerText = `Hola ${nombre}, bienvenido a Programación Visual!`;
-    } else {
-        document.getElementById("mensaje").innerText = "Por favor, ingresa tu nombre.";
-    }
-}
