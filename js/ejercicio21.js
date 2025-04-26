c4fd7314cbd0abc0d64b2c12f848fe33eba03f8b
PabloRZ-AL <rodriguezpablouski.com>
2025-04-25 23:52:42 -0300

    solo rama del ejercicio nueve

--- js/ejercicio21.js
+++
@@ -1,6 +0,0 @@
-const input = document.getElementById("entrada");
-const texto = document.getElementById("salida");
-
-input.addEventListener("input", () => {
-    texto.textContent = input.value;
-});
