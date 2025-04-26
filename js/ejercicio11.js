fcaa1b9a4da545d353158c7047d1a4fe7c7e91d4
PabloRZ-AL <rodriguezpablouski.com>
2025-04-26 00:19:00 -0300

    solo rama del ejercicion 18

--- js/ejercicio11.js
+++
@@ -1,7 +0,0 @@
-// Pedir datos al usuario
-let km = parseFloat(prompt("Kilómetros recorridos:"));
-let litros = parseFloat(prompt("Litros consumidos:"));
-
-// Calcular y mostrar resultado
-let consumo = litros / km;
-alert("Consumo por km: " + consumo + " litros/km");
