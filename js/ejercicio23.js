682e0101fa80fba549c9e48031447fb24bfcd357
EduardTr48 <tiago.edu.rodriguez@gmail.com>
2025-05-23 20:55:00 -0300

    add: ejercicio 23

---
+++ js/ejercicio23.js
@@ -0,0 +1,11 @@
+ const radios = document.querySelectorAll('input[name="lenguaje"]');
+const resultado = document.getElementById('resultado');
+
+  // A cada uno le añade un event listener para "change"
+  radios.forEach((radio) => {
+    radio.addEventListener('change', () => {
+      if (radio.checked) {
+        resultado.textContent = `Lenguaje seleccionado: ${radio.value}`;
+      }
+    });
+  });
